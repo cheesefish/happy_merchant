5dc3af69da40184fa6143248551d7930299a02da
Klaxel46 <44064032+Klaxel46@users.noreply.github.com>
2018-10-16 11:54:50 +0200

    Update class_structure.py

--- class_structure.py
+++ class_structure.py
@@ -1,33 +1,21 @@
-class Ware:
-    #ATTRIBUTES:
-    #str name       
-    #int amount
-    #int price      Determined by demand.
-    pass
+#Visualtization of class structure
 
-class Market:
-    #ATTRIBUTES:
-    #dict wares     List of Ware-objects, representing the market's supply of goods.
-    
-    def update(self, town):
-        #Updates the market supply and prices according to the town's population and industry.
-        pass
-
-class Industry:
-    #ATTRIBUTES:
-    #dict output     List of Ware-objects, representig the industry's throughput of goods.
-    pass
-
-class Town:
-    #ATTRIBUTES:
-    #str name
-    #int pop            Town population number
-    #Market market
-    #Industry industry
-    pass
-
-class World:
-    #ATTRIBUTES:
-    #dict towns
-    pass
-
+class World
+    class Town[]
+        class Market
+            class Stock[]
+                class Ware
+        class AgricultureAllocation[]
+            class Agriculture
+                class ProductionIO[] inputs
+                    class Ware
+                class ProductionIO[] outputs
+                    class Ware
+        class Business[]
+            class Industry
+                class ProductionIO[] inputs
+                    class Ware
+                class ProductionIO[] outputs
+                    class Ware
+            class Stock[]
+                class Ware
