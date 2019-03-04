f8d1971362fa9cbe54fa6d84cbc6d7bd230d65a2
Richard Uggelberg <richard.uggelberg@gmail.com>
2019-03-04 12:54:24 +0100

    Feature: maven file structure (#12)
    Add maven file structure for source code and tests. Fixes #12

---
+++ src/test/java/mainTests.java
@@ -0,0 +1,11 @@
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class mainTests {
+
+    @Test
+    public void placeholder_test_returnTrue() {
+        Assert.assertTrue(true);
+    }
+}
